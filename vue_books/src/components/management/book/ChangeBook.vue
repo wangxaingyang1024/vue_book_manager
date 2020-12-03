2569577d6c3c77f33e8204c464b08952d8a17548
lizhouweichen <a13892067220@163.com>
2020-12-03 11:22:22 +0800

    type:[增加] message:1.增加了用户登录模块初始化 2.增加书籍管理的模块

--- vue_books/src/components/management/book/ChangeBook.vue
+++ vue_books/src/components/management/book/ChangeBook.vue
@@ -1,15 +1,44 @@
 <template>
   <el-card>
     <el-row :gutter="20">
-        <el-col :span="8">
-          <el-input placeholder="请输入内容"  clearable>
-            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
-          </el-input>
-        </el-col>
-        <el-col :span="4">
-          <el-button type="primary" @click="goAddBook">添加书籍</el-button>
-        </el-col>
-      </el-row>
+      <el-col :span="8">
+        <el-input placeholder="请输入内容" clearable @clear="getBookList">
+          <el-button
+            slot="append"
+            icon="el-icon-search"
+            @click="getBookList"
+          ></el-button>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" @click="goAddBook">添加书籍</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="userlist" border style="width: 100%" :stripe="true">
+      <el-table-column type="index" label="序号" width="180"> </el-table-column>
+      <el-table-column prop="username" label="图书名" width="180">
+      </el-table-column>
+      <el-table-column prop="jobNumber" label="作者" width="180">
+      </el-table-column>
+      <el-table-column prop="nickName" label="类型" width="180">
+      </el-table-column>
+      <el-table-column prop="gender" label="状态" width="180">
+        <template slot-scope="scope">
+          {{ scope.row.gender == 1 ? "男" : "女" }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="phone" label="书号" width="180"> </el-table-column>
+      <el-table-column prop="age" label="简介" width="180"> </el-table-column>
+      <el-table-column label="操作">
+        <!-- 删除按钮 -->
+        <el-button
+          type="danger"
+          icon="el-icon-delete"
+          size="mini"
+          @click="removeBook"
+        ></el-button>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
@@ -22,12 +51,56 @@ export default {
     this.getBookList();
   },
   methods: {
-    goAddBook(){
-      this.$router.push('/addBook')
-    }
+    //跳转添加书籍路由
+    goAddBook() {
+      this.$router.push("/addBook");
+    },
+    async getBookList() {
+      const { data: res } = await this.$http.get(
+        "http://localhost:8080/api/admin/login"
+      );
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取图书列表失败！");
+      }
+      this.booklist = res.books;
+      console.log(res);
+    },
+    async removeBook(isbn) {
+      //弹框询问用户是否删除数据
+      const confirmResult = await this.$confirm(
+        "此操作将永久删除该书籍, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+      //如果用户确认删除,则返回值为字符串confirm
+      //如果用户取消删除，则返回值为字符串cancel
+      if (confirmResult !== "confirm") {
+        return this.$message.info("已取消删除");
+      }
+
+      const { data: res } = await this.$http.post(
+        "http://localhost:8080/api/admin/remove/" 
+        // `http://localhost:8080/api/admin/remove/jobNumber/`
+      );
+
+      if (res.meta.status !== 3033) {
+        return this.$message.error("删除书籍失败");
+      }
+
+      this.$message.success("删除书籍成功！");
+      this.getBookList();
+    },
   },
 };
 </script>
-
-<style>
+<style lang="less">
+.el-row {
+  margin-bottom: 25px;
+}
+</style>
+<style lang="less" scoped>
 </style>
--- vue_books/src/components/users/Home.vue
+++ vue_books/src/components/users/Home.vue
@@ -1,19 +1,94 @@
 <template>
-  <div>用户Home组件</div>
+  <el-container>
+    <el-header>
+      <span class="topText">用户已登录</span>
+      <div class="logo"><img src="../../../public/logo.svg" height="" /></div>
+      <el-button type="info" round @click="logout" class="logout"
+        >退出</el-button
+      >
+      <!-- //TODO  1.显示XX已登录 -->
+      <!-- <div>欢迎{{session.user.nick_name}}</div> -->
+    </el-header>
+    <!-- 内容主体 -->
+    <el-main class="rightMain">
+      <!-- 路由占位符 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 export default {
-  data(){
-    return{}
+  data() {
+    return {};
   },
-  created(){
-
+  created() {
+    // var nick_name = "${session.user.nick_name}";
   },
-  methods:{}
-}
+  methods: {
+    //退出
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    user() {
+      this.$router.push("/user");
+    },
+    book() {
+      this.$router.push("/changeBook");
+    },
+    addBook() {
+      this.$router.push("/addBook");
+    },
+  },
+};
 </script>
 
-<style>
+
+<style lang="less">
+.el-menu-item-group__title {
+  padding: 0 !important;
+}
+</style>
+
+
+<style lang="less" scoped>
+.el-main {
+  height: 87%;
+  width: 99%;
+  top: 77px;
+  //left: 216px;
+  position: absolute;
+  padding: 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+}
+.el-header {
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  height: 80px;
+  width: 100%;
+  position: relative;
+  z-index: 1000;
+  clear: both;
+}
+.topText {
+  font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+  font-size: 30px;
+  position: relative;
+  left: 520px;
+  top: 2px;
+}
+.logo {
+  position: absolute;
+  left: 370px;
+  top: 5px;
+}
+.logout {
+  position: absolute;
+  right: 250px;
+  top: 5px;
+}
 
 </style>
