19651d087ea9db931d2553e987a522404ffb6124
lizhouweichen <a13892067220@163.com>
2020-12-14 16:13:17 +0800

    type:[新增]
    message:
    1.前端开发环境的跨域
    2.axios的配置
    3.路由的配置

--- vue_mobile_books/src/router/index.js
+++ vue_mobile_books/src/router/index.js
@@ -3,20 +3,47 @@ import VueRouter from 'vue-router'
 import Login from '../components/users/Login.vue'
 import AdminLogin from '../components/management/AdminLogin.vue'
 import SignUp from '../components/users/SignUp.vue'
+import AdminHome from '../components/management/AdminHome.vue'
 
 
 Vue.use(VueRouter)
 
-const routes = [
-  //用户访问重定向到登录界面
-  { path:'/' , redirect:'/login' }, 
-  { path:'/login' , component:Login },
-  { path:'/adminlogin' , component:AdminLogin },
-  { path:'/signup' , component: SignUp}
-]
-
 const router = new VueRouter({
-  routes
+    routes: [
+        //用户路由
+        //用户访问重定向到登录界面
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        { path: '/signup', component: SignUp },
+        //管理员路由
+        { path: "/adminLogin", component: AdminLogin },
+        {
+            path: '/adminHome',
+            component: AdminHome,
+            redirect: '/welcome',
+            children: [
+                { path: '/welcome', component: Welcome }
+            ]
+        }
+    ]
 })
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    //to 将要访问的路径
+    //from 代表从哪个路径跳转来
+    //next 一个函数，表示放行
+    // next() 放行 next('/login') 强制跳转
+
+    if (to.path === "/login") return next();
+    if (to.path === "/signUp") return next();
+    if (to.path === "/adminLogin") return next();
+    //TODO 2.通过获取token来拦截
+
+    //获取token
+    //const tokenStr = window.localStorage.getItem('token')
+    //if (!tokenStr) return next('/login')
+    next();
+});
 
 export default router
---
+++ vue_mobile_books/vue.config.js
@@ -0,0 +1,56 @@
+const path = require("path"); //引入path模块
+function resolve(dir) {
+    return path.join(__dirname, dir); //path.join(__dirname)设置绝对路径
+}
+module.exports = {
+    devServer: {
+        host: "localhost",
+        open: true,
+        port: 8081,
+        //host: "localhost",
+        proxy: {
+            "/api": {
+                // /api 的意义在于，声明axios中url已/api开头的请求都适用于该规则，
+                // 注意是以/api开头，即：axios.post({url: '/api/xxx/xxx'})
+                target: "http://192.168.2.103:8899", //'http://192.168.6.163:8080/'
+                changeOrigin: true,
+                pathRewrite: { "^/api": "/api" },
+            },
+        },
+    },
+    chainWebpack: (config) => {
+        //配置路径别名
+        config.resolve.alias.set("assets", resolve("./src/assets"));
+        config.resolve.alias.set("components", resolve("./src/components"));
+        //发布模式
+        config.when(process.env.NODE_ENV === "production", (config) => {
+            config
+                .entry("app")
+                .clear()
+                .add("./src/main-prod.js");
+            //使用externals设置排除项
+            config.set("externals", {
+                // vue: "Vue",
+                // "vue-router": "VueRouter",
+                // axios: "axios",
+                // nprogress: "NProgress",
+            });
+            //自定义首页
+            config.plugin("html").tap((args) => {
+                args[0].isProd = true;
+                return args;
+            });
+        });
+        //开发模式
+        config.when(process.env.NODE_ENV === "development", (config) => {
+            config
+                .entry("app")
+                .clear()
+                .add("./src/main-dev.js");
+            config.plugin("html").tap((args) => {
+                args[0].isProd = false;
+                return args;
+            });
+        });
+    },
+};
