3f4513020134ed74823d4d13cd453959020e88a9
gzh <272970666@qq.com>
2020-12-16 11:42:17 +0800

    type:[修改]
    message:
    手机端登录模块接口设置

--- vue_mobile_books/src/components/Login.vue
+++ vue_mobile_books/src/components/Login.vue
@@ -10,23 +10,25 @@
     <div class="welcome">
       <p>欢迎登录均均图书</p>
     </div>
-    <van-form :model="loginForm" ref="loginFormRef">
+    <van-form @submit="login">
       <van-field
+        placeholder="请输入用户名"
         v-model="loginForm.username"
         label="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        :rules="[{ required: true }]"
       />
       <van-field
+        placeholder="请输入密码"
         v-model="loginForm.password"
         type="password"
         label="密码"
         :rules="[
-          { required: true, message: '请填写密码' },
-          { validator, message: '需包含大小写字母数字，不使用特殊字符8~15长度' }
+          { required: true },
+          { pattern, message: '需包含大小写字母数字，不使用特殊字符8~15长度' }
         ]"
       />
       <div style="margin: 35px">
-        <van-button round block type="primary" @click="login">
+        <van-button round block type="primary" native-type="submit">
           登录
         </van-button>
       </div>
@@ -43,7 +45,9 @@ export default {
       loginForm: {
         username: "zzz",
         password: "Guo123456"
-      }
+      },
+      //密码验证
+      pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/
     };
   },
   methods: {
@@ -54,46 +58,21 @@ export default {
     //跳转到注册页面
     signUp() {
       this.$router.push("/signUp");
-      //window.sessionStorage.clear();
     },
     //登录
     async login() {
-      /*let that = this;
-      that.$refs.loginFormRef.validate(async (valid) => {
-        if (!valid) return;
-        const { data: res } = await that.$http.post("login", that.loginForm);
-        if (res.status === 3031)
-          return that.$toast.fail("用户名不存在，请先注册!");
-        if (res.status !== 1000) return that.$toast.fail("登录失败!");
-        //that.$message.success("登录成功!");
-        //将登录成功的token保存到客户端的sessionStorage中
-        //console.log(res.data);
-        //window.sessionStorage.setItem("token", res.data.token);
-        //将jobNumber保存到客户端的sessionStorage中
-        window.sessionStorage.setItem("jobNumber", res.data.jobNumber);
-        window.sessionStorage.setItem("nickName", res.data.nickName);
-        // console.log(res.data.jobNumber);
-        //通过编程式导航跳转到后台主页，路由地址  /home
-        //that.$router.push("/home");
-      });*/
       const { data: res } = await this.$http.post("login", this.loginForm);
-      console.log(res);
       if (res.status === 3031)
         return this.$toast.fail("用户名不存在，请先注册!");
-      if (res.status === 3031)
-        return this.$toast.fail("用户名不存在，请先注册!");
-      this.$toast.success("登录成功!");
+      if (res.status !== 1000) return this.$toast.fail("登录失败!");
       //将登录成功的token保存到客户端的sessionStorage中
-      //console.log(res.data);
-      //window.sessionStorage.setItem("token", res.data.token);
+      window.sessionStorage.setItem("token", res.data.token);
       //将jobNumber保存到客户端的sessionStorage中
       window.sessionStorage.setItem("jobNumber", res.data.jobNumber);
       window.sessionStorage.setItem("nickName", res.data.nickName);
+      //通过编程式导航跳转到后台主页，路由地址  /home
+      this.$toast.success("登录成功!");
       this.$router.push("/home");
-    },
-    //密码验证
-    validator(val) {
-      return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/.test(val);
     }
   }
 };
