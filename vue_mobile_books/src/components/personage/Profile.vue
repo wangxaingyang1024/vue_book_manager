2209c20527cffea25bb787589386f9f543b0b374
lizhouweichen <a13892067220@163.com>
2020-12-16 15:32:15 +0800

    type:[新增]
    message:
    1.手机端修改用户信息表单

--- vue_mobile_books/src/components/personage/Profile.vue
+++ vue_mobile_books/src/components/personage/Profile.vue
@@ -1,18 +1,14 @@
 <template>
-  <van-form
-    ref="editFormRef"
-    :model="userForm"
-    :rules="editFormRules"
-    status-icon
-  >
+  <van-form status-icon>
     <van-field v-model="userForm.username" label="用户名" disabled />
     <van-field
       v-model="userForm.nickName"
       label="昵称"
       placeholder="设置您的昵称"
-      prop="nickName"
+      clearable
+      :rules="[{ pattern: nickNameRule, message: '请输入3-10位用户名' }]"
     />
-    <van-field v-model="gender" label="性别">
+    <van-field label="性别">
       <template #input>
         <van-radio-group v-model="userForm.gender" direction="horizontal">
           <van-radio name="1">男</van-radio>
@@ -20,33 +16,56 @@
         </van-radio-group>
       </template>
     </van-field>
-    <van-field
+
+    <!-- <van-field
       readonly
       clickable
+      type="date"
       name="datetimePicker"
       :value="value"
       label="时间选择"
       placeholder="点击选择时间"
       @click="showPicker = true"
-    />
-    <van-popup v-model="showPicker" position="bottom">
+    /> -->
+    <!-- <van-field> -->
+
+    <van-cell is-link @click="showPopup"
+      ><span class="birth">生日</span>
+      <span>{{
+        this.currentDate.getFullYear() +
+        "-" +
+        this.currentDate.getMonth() +
+        "-" +
+        this.currentDate.getDate()
+      }}</span></van-cell
+    >
+    <van-popup
+      v-model="show"
+      position="bottom"
+      round
+      @confirm="success"
+      @cancel="info"
+    >
       <van-datetime-picker
-        type="time"
-        @confirm="onConfirm"
-        @cancel="showPicker = false"
+        v-model="currentDate"
+        type="date"
+        title="选择年月日"
+        :min-date="minDate"
+        :max-date="maxDate"
       />
     </van-popup>
+    <!-- </van-field> -->
     <van-field
-      v-model="value1"
+      v-model="userForm.phone"
       label="手机"
-      name="pattern"
+      name="phone"
       placeholder="输入您的手机"
-      :rules="[{ pattern, message: '请输入正确内容' }]"
+      type="tel"
+      clearable
+      :rules="[{ pattern, message: '请输入正确手机号' }]"
     />
     <div style="margin: 16px">
-      <van-button round block type="info" native-type="submit">
-        提交
-      </van-button>
+      <van-button round block type="info" @click="editUser"> 提交 </van-button>
     </div>
   </van-form>
 </template>
@@ -54,94 +73,73 @@
 <script>
 export default {
   data() {
-    //验证手机规则
-    const checkPhone = (rule, value, callback) => {
-      if (value === "") {
-        return callback();
-      }
-      const regPhone = /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/;
-
-      if (regPhone.test(value)) {
-        //合法手机
-        return callback();
-      }
-      callback(new Error("请输入合法手机号"));
-    };
     return {
-      value: "",
-      showPicker: false,
+      show: false,
+      nickNameRule: /^.{3,10}$/,
+      minDate: new Date(1900, 0, 1),
+      maxDate: new Date(2020, 10, 1),
+      currentDate: new Date(),
+      pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,
+      // value: "",
+      // showPicker: false,
       jobNumber: window.sessionStorage.getItem("jobNumber"),
-      userForm: {},
-      editFormRules: {
-        nickName: [
-          {
-            min: 3,
-            max: 10,
-            message: "长度3~10之间",
-            trigger: "blur",
-          },
-        ],
-        //验证手机
-        phone: [
-          {
-            validator: checkPhone,
-            trigger: "blur",
-          },
-        ],
+      userForm: {
+        nickName: "",
+        gender: "",
       },
-      pickerOptions: {
-        disabledDate(time) {
-          return time.getTime() >= Date.now();
-        },
-      },
+      // pickerOptions: {
+      //   disabledDate(time) {
+      //     return time.getTime() >= Date.now();
+      //   },
+      // },
     };
   },
   created() {
     this.getUserForm();
   },
   methods: {
-    onConfirm(time) {
-      this.value = time;
-      this.showPicker = false;
+    showPopup() {
+      this.show = true;
+    },
+    success() {
+      console.log(111);
+    },
+    info() {
+      console.log(222);
     },
     async getUserForm() {
       const { data: res } = await this.$http.get(`profile/${this.jobNumber}`);
 
       console.log(res.data);
       if (res.status !== 200) {
-        return this.$message.error("获取个人信息失败！");
+        return this.$toast.fail("获取个人信息失败！");
       }
       this.userForm = res.data;
     },
     //点击按钮提交表单
-    editUser() {
-      this.$refs.editFormRef.validate(async (valid) => {
-        if (!valid) return;
-        const confirmResult = await this.$confirm("是否确认修改?", "提示", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-        }).catch((err) => err);
-        if (confirmResult === "confirm") {
-          const { data: res } = await this.$http.put(
-            "upProfile",
-            this.userForm
-          );
-          if (res.status !== 3032) {
-            return this.$message.error("修改失败！");
-          } else {
-            location.reload();
-            // this.getUserForm();
-            this.$message.success("修改成功！");
-            console.log(this.userForm.nickName);
-            window.sessionStorage.setItem("nickName", this.userForm.nickName);
-          }
-        }
-      });
+    async editUser() {
+      const { data: res } = await this.$http.put("upProfile", this.userForm);
+      if (res.status !== 3032) {
+        return this.$toast.fail("修改失败！");
+      } else {
+        location.reload();
+        // this.getUserForm();
+        this.$toast.success("修改成功！");
+        console.log(this.userForm.nickName);
+        window.sessionStorage.setItem("nickName", this.userForm.nickName);
+      }
     },
   },
+  // watch: {
+  //   currentDate(val) {
+  //     console.log(val);
+  //   },
+  // },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.birth {
+  margin-right: 4.3rem;
+}
 </style>
--- vue_mobile_books/src/plugins/vant.js
+++ vue_mobile_books/src/plugins/vant.js
@@ -1,18 +1,5 @@
-import Vue from 'vue'
-import { Button, NavBar, Icon, Form, Field, Toast, Popover, Grid, GridItem, Card, Search, Collapse, CollapseItem, Radio, RadioGroup, DatetimePicker } from 'vant'
+import Vue from 'vue';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
-Vue.use(Button)
-Vue.use(NavBar)
-Vue.use(Icon)
-Vue.use(Form)
-Vue.use(Field)
-Vue.use(Popover)
-Vue.use(Grid)
-Vue.use(GridItem)
-Vue.use(Card)
-Vue.use(Search)
-Vue.use(Collapse)
-Vue.use(CollapseItem)
-Vue.use(Radio)
-Vue.use(RadioGroup)
-Vue.use(DatetimePicker)
+Vue.use(Vant);
