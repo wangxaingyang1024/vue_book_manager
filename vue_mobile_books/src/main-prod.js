48f7f0e966cb29e5b089e7a23aca58cb763b6b9e
lizhouweichen <a13892067220@163.com>
2020-12-17 18:05:43 +0800

    type:[修改]
    message:
    通过cdn加载外部资源优化打包

--- vue_mobile_books/src/main-prod.js
+++ vue_mobile_books/src/main-prod.js
@@ -1,22 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./plugins/vant.js";
 //导入全局样式表
 import "./assets/css/global.css";
 //引入axios
 import axios from "axios";
-//引入nprogress包对应的js和css
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+
 //配置请求根路径
 axios.defaults.baseURL = "http://localhost:8081/api/";
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
 axios.interceptors.request.use(config => {
-  config.headers.Authorization = window.sessionStorage.getItem("token");
-  return config;
+    config.headers.Authorization = window.sessionStorage.getItem("token");
+    return config;
 });
 
 NProgress.inc(0.2);
@@ -24,17 +21,17 @@ NProgress.configure({ easing: "ease", speed: 500, showSpinner: false });
 
 //在request拦截器中，展示进度条
 router.beforeEach((to, from, next) => {
-  NProgress.start();
-  next();
+    NProgress.start();
+    next();
 });
 
 // //在request拦截器中，隐藏进度条
 router.afterEach(() => {
-  NProgress.done();
+    NProgress.done();
 });
 
 new Vue({
-  el: "#app",
-  router,
-  render: h => h(App)
+    el: "#app",
+    router,
+    render: h => h(App)
 }).$mount("#app");
