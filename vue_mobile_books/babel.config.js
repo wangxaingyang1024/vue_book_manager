7912f2d5afdb64fd30ac39c860f7f72ea4f9447b
lizhouweichen <a13892067220@163.com>
2020-12-17 17:55:08 +0800

    type:[修改]
    message:
    在发布阶段移除console

--- vue_mobile_books/babel.config.js
+++ vue_mobile_books/babel.config.js
@@ -1,5 +1,12 @@
+//这是 项目发布阶段需要用到的babel插件
+const prodPlugins = [];
+if (process.env.NODE_ENV === "production") {
+    prodPlugins.push("transform-remove-console");
+}
+
 module.exports = {
     presets: [
         '@vue/cli-plugin-babel/preset'
     ],
+    plugins: [...prodPlugins],
 }
