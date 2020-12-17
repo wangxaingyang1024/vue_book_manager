7b3a20ad9e26e348bc2ea22d9ab2a77d53ffd7ed
gzh <272970666@qq.com>
2020-12-17 17:26:11 +0800

    type:[修改]
    message:
    1.配置路径别名
    2.添加进度条效果

--- vue_mobile_books/src/router/index.js
+++ vue_mobile_books/src/router/index.js
@@ -1,56 +1,55 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from "../components/Login.vue";
-import SignUp from "../components/SignUp.vue";
-import Home from "../components/Home.vue";
-import Admin from "../components/personage/Admin.vue";
-import Profile from "../components/personage/Profile.vue";
-import Person from "../components/personage/Person.vue";
-import Setting from "../components/personage/Setting.vue";
-import MyBooks from "../components/books/MyBooks.vue";
-import Books from "../components/books/Books.vue";
-
+import Login from "components/Login.vue";
+import SignUp from "components/SignUp.vue";
+import Home from "components/Home.vue";
+import Admin from "components/personage/Admin.vue";
+import Profile from "components/personage/Profile.vue";
+import Person from "components/personage/Person.vue";
+import Setting from "components/personage/Setting.vue";
+import MyBooks from "components/books/MyBooks.vue";
+import Books from "components/books/Books.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    routes: [
-        //用户路由
-        //用户访问重定向到登录界面
-        { path: "/", redirect: "/books" },
-        { path: "/login", component: Login },
-        { path: "/signUp", component: SignUp },
-        { path: "/setting", component: Setting },
-        { path: "/admin", component: Admin },
-        { path: "/profile", component: Profile },
-        {
-            path: "/home",
-            component: Home,
-            redirect: "/books",
-            children: [
-                { path: "/myBooks", component: MyBooks },
-                { path: "/books", component: Books },
-                { path: "/person", component: Person },
-            ]
-        }
-    ]
+  routes: [
+    //用户路由
+    //用户访问重定向到登录界面
+    { path: "/", redirect: "/books" },
+    { path: "/login", component: Login },
+    { path: "/signUp", component: SignUp },
+    { path: "/setting", component: Setting },
+    { path: "/admin", component: Admin },
+    { path: "/profile", component: Profile },
+    {
+      path: "/home",
+      component: Home,
+      redirect: "/books",
+      children: [
+        { path: "/myBooks", component: MyBooks },
+        { path: "/books", component: Books },
+        { path: "/person", component: Person }
+      ]
+    }
+  ]
 });
 
 //挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-    //to 将要访问的路径
-    //from 代表从哪个路径跳转来
-    //next 一个函数，表示放行
-    // next() 放行 next('/login') 强制跳转
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转来
+  //next 一个函数，表示放行
+  // next() 放行 next('/login') 强制跳转
 
-    if (to.path === "/login") return next();
-    if (to.path === "/signUp") return next();
-    //TODO 2.通过获取token来拦截
+  if (to.path === "/login") return next();
+  if (to.path === "/signUp") return next();
+  //TODO 2.通过获取token来拦截
 
-    //获取token
-    //const tokenStr = window.localStorage.getItem('token')
-    //if (!tokenStr) return next('/login')
-    next();
+  //获取token
+  //const tokenStr = window.localStorage.getItem('token')
+  //if (!tokenStr) return next('/login')
+  next();
 });
 
 export default router;
