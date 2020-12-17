ac157e527aa1f2cbc94cb217bad4f0653816a263
gzh <272970666@qq.com>
2020-12-17 09:45:49 +0800

    type:[修改]
    message:
    配置在请求头中加入token

--- vue_books/vue.config.js
+++ vue_books/vue.config.js
@@ -4,14 +4,15 @@ function resolve(dir) {
 }
 module.exports = {
   devServer: {
-    host: "localhost",
+    host:
+      process.env.NODE_ENV === "production" ? "127.0.0.1:3000" : "localhost",
+    // host: "localhost",
     open: true,
-    //host: "localhost",
     proxy: {
       "/api": {
         // /api 的意义在于，声明axios中url已/api开头的请求都适用于该规则，
         // 注意是以/api开头，即：axios.post({url: '/api/xxx/xxx'})
-        target: "http://192.168.2.103:8899", //'http://192.168.6.163:8080/'
+        target: "http://182.92.77.213:8899", //'http://192.168.6.163:8080/'
         changeOrigin: true,
         pathRewrite: { "^/api": "/api" },
       },
